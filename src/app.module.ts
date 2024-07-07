1d6bcfec2f95093c150502d1bb6d717638e4b685
Arh <Arh@DESKTOP-8DSVULN>
2024-07-07 10:33:42 +0330

    graphql init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CoffeesModule } from './coffees/coffees.module';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { CoffeesModule } from './coffees/coffees.module';
 import { CommonModule } from './common/common.module';
-import { UsersModule } from './users/users.module';
-import { AuthModule,  } from './auth/auth.module';
 import ormConfig from './config/orm.config';
 import ormConfigProd from './config/orm.config.prod';
 import validationSchema from './config/validation-schema';
+import { UsersModule } from './users/users.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
     imports: [
@@ -26,6 +29,10 @@ import validationSchema from './config/validation-schema';
                 process.env.NODE_ENV !== 'production'
                     ? ormConfig
                     : ormConfigProd,
+        }),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
         }),
         CoffeesModule,
         CommonModule,
